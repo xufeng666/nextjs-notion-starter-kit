84c1ea7a92957eececc4d191b5c9791bb1b73801
看啥呢，我瞅瞅 <1836992695@qq.com>
2021-08-23 21:26:25 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '7965f3dc90964842917c7d93dbc87a3a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -8,8 +8,8 @@ module.exports = {
 
   // basic site info (required)
   name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'https://www.notion.so/xufeng123/7965f3dc90964842917c7d93dbc87a3a',
+  author: 'Xu Feng',
 
   // open graph metadata (optional)
   description: 'Example site description',
